7229c08526dc7698c7996b04daa72a5ba776ec42
ahmedali555 <ahmedali5551@gmail.com>
2016-04-25 17:41:40 +0500

    1ST COMMIT......!!!!!!!!!!!!!!!!!!!!!!

---
+++ VersionControl/src/GitMain.java
@@ -0,0 +1,13 @@
+
+public class GitMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("WELCOME \n TO MY \n GITHUB");
+		
+		System.out.println("...............\n .................\n..............");
+
+	}
+
+}
